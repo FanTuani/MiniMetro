67354ca7cdba6f7d56d123dbd0705c3ffe7701e8
RiceQuakes <2486921393@qq.com>
2024-12-04 15:49:16 +0800

    Update navigation.h

--- src/navigation.h
+++ src/navigation.h
@@ -7,4 +7,16 @@ void getLinesInfo();
 
 void navigate(const string &st, const string &des);
 
+int calculateRouteTime(const vector<pair<string, int> > &rou);
+
+int calculateRouteTransfer(const vector<pair<string, int> > &rou);
+
+bool timeCmp(const vector<pair<string, int> > &a, const vector<pair<string, int> > &b);
+
+bool transferCmp(const vector<pair<string, int> > &a, const vector<pair<string, int> > &b);
+
+bool stationCmp(const vector<pair<string, int> > &a, const vector<pair<string, int> > &b);
+
+void printRouteInfo(const vector<pair<string, int> > &rou);
+
 #endif //MINIMETRO_NAVIGATION_H
